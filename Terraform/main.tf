4102b8dd6229d9eb6c44fea727d067c60e428a1a
zaval13 <61337346+zaval13@users.noreply.github.com>
2021-07-26 14:28:49 +0300

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -49,6 +49,14 @@ resource "aws_security_group" "my_webserver1" {
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
+  
+  ingress {
+    from_port        = 3000
+    to_port          = 3000
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  
 
   egress {
     from_port        = 0
